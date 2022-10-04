e7b2c9eeb8ed8005135d24a32372be89ff52e22d
Niel Kakkad <niel.kakkad@gmail.com>
2022-10-04 17:52:47 -0400

    Nothing

---
+++ capstone/vue/src/components/ReviewDisplay.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="ReviewContainer">
+        <div v-for="review in this.$store.state.reviews" v-bind:key="review.reviewId">
+            <div id="review-info">
+                <h3>Rating: {{ review.rating }}</h3>
+                <p>Review: {{ review.description }}</p>
+            </div>
+        </div>
+    </div>
+</template>
